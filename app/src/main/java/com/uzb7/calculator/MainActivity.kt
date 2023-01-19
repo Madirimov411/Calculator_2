854cde7c4a62dca31266edcfa263b5eed2d000dd
data <madirimov411@gmail.com>
2023-01-19 19:00:57 +0500

    Yangilandi3

--- app/src/main/java/com/uzb7/calculator/MainActivity.kt
+++ app/src/main/java/com/uzb7/calculator/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.uzb7.calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var displey:EditText
+    lateinit var displey:TextView
     lateinit var natija:TextView
     var numbertext:String=""
     var textnumber:Int=0
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         initview()
     }
 
+    @SuppressLint("WrongViewCast")
     private fun initview() {
         val bone=findViewById<Button>(R.id.b_1)
         val btwo=findViewById<Button>(R.id.b_2)
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val bnine=findViewById<Button>(R.id.b_9)
         val bzero=findViewById<Button>(R.id.b_0)
         val bAC=findViewById<Button>(R.id.b_c)
+        val bOchirish=findViewById<Button>(R.id.b_ochirish)
         val bpilusminus=findViewById<Button>(R.id.b_plus_minus)
         val bplus=findViewById<Button>(R.id.b_qoshish)
         val bminus=findViewById<Button>(R.id.b_ayirish)
@@ -38,7 +40,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val bbolish=findViewById<Button>(R.id.b_bolish)
         val bbarobar=findViewById<Button>(R.id.b_teng)
         natija=findViewById(R.id.tv_javob)
-        displey=findViewById(R.id.et_kiritish)
+        displey=findViewById(R.id.tv_kiritish)
         bone.setOnClickListener(this)
         btwo.setOnClickListener(this)
         bthree.setOnClickListener(this)
@@ -56,6 +58,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         bnine.setOnClickListener(this)
         bzero.setOnClickListener(this)
         bAC.setOnClickListener(this)
+        bOchirish.setOnClickListener(this)
         bpilusminus.setOnClickListener(this)
         bplus.setOnClickListener(this)
         bminus.setOnClickListener(this)
@@ -67,45 +70,37 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(button: View?) {
         val id=button?.id
         when(id){
-            R.id.b_1->{
-                numbertext+="1"
-                displey.setText(numbertext)
+            R.id.b_1->{ yozish("1") }
+            R.id.b_2->{ yozish("2") }
+            R.id.b_3->{ yozish("3") }
+            R.id.b_4->{ yozish("4") }
+            R.id.b_5->{ yozish("5") }
+            R.id.b_6->{ yozish("6") }
+            R.id.b_7->{ yozish("7") }
+            R.id.b_8->{ yozish("8") }
+            R.id.b_9->{ yozish("9") }
+            R.id.b_0->{ yozish("0") }
+            R.id.b_c->{
+                natija.text=""
+                displey.text=""
             }
-            R.id.b_2->{
-                numbertext+="2"
-                displey.setText(numbertext)
+            R.id.b_ochirish->{
+                var s=displey.text
+                var a = ""
+                displey.text=""
+                for(i in 0 until s.length-1){
+                    a+=s[i]
+                }
+                displey.text = a
             }
-            R.id.b_3->{
-                numbertext+="3"
-                displey.setText(numbertext)
-            }
-            R.id.b_4->{
-                numbertext+="4"
-                displey.setText(numbertext)
-            }
-            R.id.b_5->{
-                numbertext+="5"
-                displey.setText(numbertext)
-            }
-            R.id.b_6->{
-                numbertext+="6"
-                displey.setText(numbertext)
-            }
-            R.id.b_7->{
-                numbertext+="7"
-                displey.setText(numbertext)
-            }
-            R.id.b_8->{
-                numbertext+="8"
-                displey.setText(numbertext)
-            }R.id.b_9->{
-            numbertext+="9"
-            displey.setText(numbertext)
-            }
-            R.id.b_0->{
-            numbertext+="0"
-            displey.setText(numbertext)
-            }
+        }
+    }
+
+    private fun yozish(s: String) {
+        numbertext+=s
+        displey.text=numbertext
+        if(!displey.text.toString().contains("+")&&!displey.text.toString().contains("-")&&!displey.text.toString().contains("*")&&!displey.text.toString().contains("/")){
+            natija.text=displey.text
         }
     }
 }
